7b0f10c21d7014fd545a8aeb5e22d89c13c70b63
itjunky <itjunky@jeff.ws>
2016-11-27 14:37:28 +0300

    Add categories

--- app/parse_freelansim.py
+++ app/parse_freelansim.py
@@ -17,8 +17,8 @@ from BeautifulSoup import BeautifulSoup          # Для обработки HTM
 default_url = "https://freelansim.ru/tasks/"
 admin_url = "https://freelansim.ru/tasks?categories=admin_network,admin_servers,admin_databases,admin_design,admin_testing,admin_other"
 webdev_url = "https://freelansim.ru/tasks?categories=web_programming,web_prototyping,web_test"
-page = urllib2.urlopen(admin_url)
-soup = BeautifulSoup(page)
+webdis_url = "https://freelansim.ru/tasks?categories=web_design,web_html"
+
 
 def job_exist(job_link):
     cur = session.execute("SELECT url FROM job")
@@ -27,32 +27,38 @@ def job_exist(job_link):
     if any(job_link in s[0] for s in links): return True
     else: return False
 
+def parse_category(url, category):
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page)
+    all_jobs = soup.findAll('article', {"class": "task task_list"})
 
-all_jobs = soup.findAll('article', {"class": "task task_list"})
+    for job in all_jobs:
 
-for job in all_jobs:
+        title = job.find("div", "task__title").text
+        print "Title:\t", title
+        url = 'http://freelansim.ru'+job.find("div", "task__title").find("a").get('href')
+        print "Url:\t", url
+        date = job.find("span", "params__published-at").text.splitlines()
+        date = str(date[0]+' '+date[1])
+        print "Date:\t", date
+        price_raw = job.find("div", "task__price")
+        price = price_raw.find("span", "count")
+        if price:
+            price = price.text
+        else: # if not exist, find another tag
+            price = price_raw.find("span", "negotiated_price").text
 
-    title = job.find("div", "task__title").text
-    print "Title:\t", title
-    url = 'http://freelansim.ru'+job.find("div", "task__title").find("a").get('href')
-    print "Url:\t", url
-    date = job.find("span", "params__published-at").text.splitlines()
-    date = str(date[0]+' '+date[1])
-    print "Date:\t", date
-    price_raw = job.find("div", "task__price")
-    price = price_raw.find("span", "count")
-    if price:
-        price = price.text
-    else: # if not exist, find another tag
-        price = price_raw.find("span", "negotiated_price").text
-
-    print "Price:\t", price, "\n"
-    #raw = job.contents
-    category = 'admin'
+        print "Price:\t", price, "\n"
+        #raw = job.contents
+        # category = 'admin'
 
     if not job_exist(url):
         sql = "INSERT INTO job (title, date, price, url, category) \
                 VALUES ('{}', '{}', '{}', '{}', '{}');".format(title, date, price, url, category)
         session.execute(sql)
 
-session.commit()
+    session.commit()
+
+parse_category(admin_url, 'admin')
+parse_category(webdev_url, 'webdev')
+parse_category(webdis_url, 'webdis')
--- bot/bot.py
+++ bot/bot.py
@@ -21,12 +21,34 @@ def handle_list(message):
 
 @bot.message_handler(commands=['freelansim', 'f'])
 def handle_freelansim(message):
-    cur = session.execute("SELECT * FROM job LIMIT 3")
+    output = '/freelansim_adm - Работа для админв\n'+\
+             '/freelansim_webdev - Работа для веб-разраба\n'+\
+             "/freelansim_webdis - Работа для веб-дизайнера"
+    bot.send_message(message.chat.id, output)
+
+@bot.message_handler(commands=['freelansim_adm', 'fa'])
+def handle_freelansim_adm(message):
+    cur = session.execute("SELECT * FROM job WHERE category = 'admin' LIMIT 3")
+    jobs = cur.fetchall()
+    for job in jobs:
+        output = '*'+str(job.id)+'* '+job[4]+'\n   '+job[2]+'\n   '+job[3]
+        bot.send_message(message.chat.id, output)
+
+@bot.message_handler(commands=['freelansim_webdev', 'fw'])
+def handle_freelansim_webdev(message):
+    cur = session.execute("SELECT * FROM job WHERE category = 'webdev' LIMIT 3")
     jobs = cur.fetchall()
     for job in jobs:
         output = str(job.id)+' '+job[4]+'\n   '+job[2]+'\n   '+job[3]
         bot.send_message(message.chat.id, output)
 
+@bot.message_handler(commands=['freelansim_webdis', 'fwd'])
+def handle_freelansim_webdis(message):
+    cur = session.execute("SELECT * FROM job WHERE category = 'webdis' LIMIT 3")
+    jobs = cur.fetchall()
+    for job in jobs:
+        output = str(job.id)+' '+job[4]+'\n   '+job[2]+'\n   '+job[3]
+        bot.send_message(message.chat.id, output)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
