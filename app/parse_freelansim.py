dc38c9188531861da0a76d8c4db02a4fd34d004e
itjunky <itjunky@jeff.ws>
2016-11-28 07:20:43 +0300

    Add dev category

--- app/parse_freelansim.py
+++ app/parse_freelansim.py
@@ -6,8 +6,8 @@ sys.setdefaultencoding('utf-8')
 
 from db import *
 
-import urllib2
-import re
+import urllib2, re
+from datetime import datetime
 
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
@@ -18,13 +18,15 @@ default_url = "https://freelansim.ru/tasks/"
 admin_url = "https://freelansim.ru/tasks?categories=admin_network,admin_servers,admin_databases,admin_design,admin_testing,admin_other"
 webdev_url = "https://freelansim.ru/tasks?categories=web_programming,web_prototyping,web_test"
 webdis_url = "https://freelansim.ru/tasks?categories=web_design,web_html"
-
+dev_url = "https://freelansim.ru/tasks?categories=app_all_inclusive,app_scripts,app_bots,app_plugins,app_utilites,app_design,app_programming,app_prototyping,app_1c_dev,app_test,app_other"
 
 def job_exist(job_link):
     cur = session.execute("SELECT url FROM job")
     links = cur.fetchall()
     # print links
-    if any(job_link in s[0] for s in links): return True
+    if any(job_link in s[0] for s in links):
+        print '-Job in DataBase-'
+        return True
     else: return False
 
 def parse_category(url, category):
@@ -52,13 +54,20 @@ def parse_category(url, category):
         #raw = job.contents
         # category = 'admin'
 
-    if not job_exist(url):
-        sql = "INSERT INTO job (title, date, price, url, category) \
-                VALUES ('{}', '{}', '{}', '{}', '{}');".format(title, date, price, url, category)
-        session.execute(sql)
+        if not job_exist(url):
+            job_row = Job(
+                            title = unicode(title),
+                            date = unicode(date),
+                            price = price,
+                            url = url,
+                            category = category,
+                            parse_date = datetime.now()
+            )
+            session.add(job_row)
 
     session.commit()
 
 parse_category(admin_url, 'admin')
 parse_category(webdev_url, 'webdev')
 parse_category(webdis_url, 'webdis')
+parse_category(dev_url, 'dev')
