45fcafa8e98b999cc132e8fb8bf109e60d6dbdd0
itjunky <itjunky@jeff.ws>
2016-12-11 02:19:01 +0300

    Parse webdev category on freelance.com

--- app/parse_freelance_com.py
+++ app/parse_freelance_com.py
@@ -17,7 +17,8 @@ session = Session()
 
 admin_url = 'http://www.freelance.com/en/search/mission?filter.searchLanguageCode=EN&filter.location.country=&filter.location.region1=&filter.location.city=&filter.duration=ALL&missionStartDateYear=2016&missionStartDateMonth=11&query='
 webdis_url = 'http://www.freelance.com/en/search/mission?filter.searchLanguageCode=EN&filter.location.country=&filter.location.region1=&filter.location.city=&filter.duration=ALL&missionStartDateYear=2016&missionStartDateMonth=11&selSkills%5B0%5D=261&selSkills%5B1%5D=259&selSkills%5B2%5D=264&selSkills%5B3%5D=265&selSkills%5B4%5D=242&selSkills%5B5%5D=191&selSkills%5B6%5D=180&query='
-dev_url = 'http://www.freelance.com/en/search/mission?filter.searchLanguageCode=EN&filter.location.country=&filter.location.region1=&filter.location.city=&filter.duration=ALL&missionStartDateYear=2016&missionStartDateMonth=11&selSkills%5B0%5D=176&selSkills%5B1%5D=255&selSkills%5B2%5D=200&selSkills%5B3%5D=173&selSkills%5B4%5D=253&selSkills%5B5%5D=225&selSkills%5B6%5D=252&query='
+dev_url = 'http://www.freelance.com/en/search/mission?filter.searchLanguageCode=EN&filter.location.country=&filter.location.region1=&filter.location.city=&filter.duration=ALL&missionStartDateYear=2016&missionStartDateMonth=11&selSkills%5B0%5D=176&selSkills%5B1%5D=255&selSkills%5B2%5D=200&selSkills%5B3%5D=173&selSkills%5B4%5D=253&selSkills%5B5%5D=225&selSkills%5B6%5D=252&query=&page=1&order=1&sort=POST_DATE'
+webdev_url = 'http://www.freelance.com/en/search/mission?filter.searchLanguageCode=EN&filter.location.country=&filter.location.region1=&filter.location.city=&filter.duration=ALL&missionStartDateYear=2016&missionStartDateMonth=12&selSkills%5B0%5D=181&query='
 
 def parse_category(url, category):
     page = urllib2.urlopen(admin_url)
@@ -52,3 +53,4 @@ def parse_category(url, category):
 parse_category(admin_url, 'admin')
 parse_category(webdis_url, 'webdis')
 parse_category(dev_url, 'dev')
+parse_category(webdev_url, 'webdev')
