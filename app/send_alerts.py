e97a487594a87e0d04f216e5ca177ac30294bb57
parcelancer <parcelancer@Hosting>
2017-04-05 02:45:59 +0400

    Change send_alerts.py to support python3 and refactor sqlalchemy usage

--- app/send_alerts.py
+++ app/send_alerts.py
@@ -2,101 +2,100 @@
 
 import json
 import config
-import urllib, urllib2
-OPENER = urllib2.build_opener()
+import telebot
+bot = telebot.TeleBot(config.token)
 
 from time import sleep
-#https://api.telegram.org/bot{id}/sendMessage?chat_id={user_id}&text={text}&disable_web_page_preview
 
 from db import *
 from sqlalchemy.orm import sessionmaker, scoped_session
 session = scoped_session(sessionmaker(bind=engine))
+from sqlalchemy import desc
 
 
 # TODO Получить список юзеров
-cur = session.execute("SELECT * FROM users")
-users = cur.fetchall()
-print(users)
+users = session.query(User).all()
+# from IPython import embed; embed()
 
 # У каждого пользователя
 for user in users:
-    # Получить последнюю работу
-    # Проверить появилась ли в его категории более новая работа
-    username = user[1]
-    print('Jobs for ' + str(username))
-    print('Last job id: ' + str(user[5]))
-    user_tele_id = user[4]
-    saved_last_job_id = user[5]
-    category = user[6]
-    try:
-        cur = session.execute("SELECT id FROM job WHERE category = '{}' ORDER BY id DESC LIMIT 100".format(category))
-        print(cur.fetchone()[0])
-        last_job_in_category = cur.fetchone()[0]
-    except TypeError:
-        print 'All categories'
+
+    print('\nNAME:\t\t', user.name)
+    print('ID:\t\t', user.tele_id)
+    print('LAST JOB\t', user.last_job)
+    print('CATEGORY:\t', user.category)
+    
+    if user.category == 'unsubscribed' or user.category == 'deleted': continue
+
+    try: # Получить последнюю работу
+        # from IPython import embed; embed()
+        last_job_in_category = session.query(Job).\
+                               filter(Job.category == user.category).\
+                               order_by(Job.id.desc()).first().id
+        print('LAST IN CATEGORY', last_job_in_category)
+    # except TypeError:
+    except AttributeError:
+        print('All categories')
         category = 'all'
         cur = session.execute("SELECT id FROM job ORDER BY id DESC")
         print(cur.fetchone()[0])
         last_job_in_category = cur.fetchone()[0]
-
-    if saved_last_job_id < last_job_in_category:
+        
+    # Проверить появилась ли в его категории более новая работа
+    if user.last_job < last_job_in_category:
         # Если в категории появилась новая работа, то отправить юзеру её
-        if category == 'all':
+        if user.category == 'all':
             cur = session.execute("SELECT id, title, parse_date, price, url, category \
                                    FROM job \
                                    WHERE id > {} \
                                    ORDER BY date(parse_date) \
-                                   LIMIT 3".format(saved_last_job_id))
+                                   LIMIT 3".format(user.last_job))
         else:
             cur = session.execute("SELECT id, title, parse_date, price, url \
                                    FROM job \
                                    WHERE category = '{}' AND id > {} \
                                    ORDER BY date(parse_date) \
-                                   LIMIT 3".format(category, saved_last_job_id))
+                                   LIMIT 3".format(user.category, user.last_job))
         jobs = cur.fetchall()
+
         for job in jobs:
-            job_text = " {} <b>{}</b>".format(str(job[0]),job[1].encode('utf-8').strip()) + \
+            job_text = " {} <b>{}</b>".format(str(job[0]),job[1].strip()) + \
                        "\n    🕑 {}".format(job[2]) + \
-                       "\n    💰 {}".format(job[3].encode('utf-8').strip()) + \
+                       "\n    💰 {}".format(job[3].strip()) + \
                        "\n    🕸 {}".format(job[4].strip())
 
-            url = "https://api.telegram.org/bot{}/sendMessage".format(config.token)
-            req = urllib2.Request(url)
-            req.add_header("Accept", "application/json")
-            req.add_header('User-agent', 'FreeLance Bot')
+            try: # Отправить работу юзеру
+                bot.send_message(user.tele_id, job_text, parse_mode='HTML', disable_web_page_preview=True)
+                print(job_text)
+            except telebot.apihelper.ApiException as e:
+                print('HTTP ERROR -', e.result.text)
+                if '403' in e.result.text:
+                    # Если юзер отрубил бота, пометить в базе и не пытаться слать
+                    # Поменяв категорию на unsubscribed
+                    print('NEED UNSUBSCRIBE for:', user.tele_id)
+                    session.query(User).\
+                            filter(User.tele_id == user.tele_id).\
+                            update({'category': 'unsubscribed'})
+                if '400' in e.result.text:
+                    # Если юзера больше не существует
+                    print('NEED DELETE for:', user.tele_id)
+                    session.query(User).\
+                            filter(User.tele_id == user.tele_id).\
+                            update({'category': 'deleted'})
 
-            data = { 'chat_id': user_tele_id,
-                     'disable_web_page_preview': 'true',
-                     'parse_mode': 'HTML',
-                     'text': job_text
-                   }
-            req.add_data(urllib.urlencode(data))
-            # req.add_data(json.dumps(data))
+                # session.commit()
+            last_id = job[0]
+            print('SENDING: ', last_id, job[4], '\n')
 
-            try:
-                last_id = job[0]
-                print 'SENDING: ', last_id, url, '\n', job_text
-            except:
-                cur = session.execute("SELECT id \
-                                       FROM job \
-                                       ORDER BY id DESC \
-                                       LIMIT 1")
-                #last_id =  cur.fetchone()[0]
-                last_id = saved_last_job_id
-                print "Failed getting last job id", last_id
+            # TODO Поменять ему последнюю работу
+            sql = "UPDATE users SET last_job = '{}' \
+                   WHERE tele_id = '{}'".format(last_id, user.tele_id)
+            session.execute(sql)
+            session.commit()
 
-            try:
-                OPENER.open(req).read()
-                # TODO Поменять ему последнюю работу
-                sql = "UPDATE users SET last_job = '{}' \
-                       WHERE tele_id = '{}'".format(last_id, user_tele_id)
-                session.execute(sql)
-                session.commit()
-            except urllib2.HTTPError, e:
-                print 'HTTP ERROR -', e
-            finally:
-                session.close()
-            # pause
-            sleep(0.300)
+    session.close()
 
+            # # pause
+            # sleep(0.300)
 
+    # break # for sending only to me
