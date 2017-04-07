2733c2d33bc0c21adca1827f0fbb2cb8b5245a10
parcelancer <parcelancer@Hosting>
2017-04-07 23:00:19 +0400

    Added categories for freelancehunt.com

--- app/parse_freelancehunt.py
+++ app/parse_freelancehunt.py
@@ -12,7 +12,10 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
-admin_url = 'https://freelancehunt.com/projects?skills%5B%5D=22&skills%5B%5D=48&skills%5B%5D=6&skills%5B%5D=39'
+admin_url = 'https://freelancehunt.com/projects?skills%5B%5D=22&skills%5B%5D=86&skills%5B%5D=83&skills%5B%5D=78&skills%5B%5D=48&skills%5B%5D=6&skills%5B%5D=39'
+dev_url = 'https://freelancehunt.com/projects?skills%5B%5D=24&skills%5B%5D=2&skills%5B%5D=5&skills%5B%5D=54&skills%5B%5D=13&skills%5B%5D=1&skills%5B%5D=22&skills%5B%5D=23&skills%5B%5D=160&skills%5B%5D=88&skills%5B%5D=169&skills%5B%5D=103&skills%5B%5D=85'
+webdev_url = 'https://freelancehunt.com/projects?skills%5B%5D=28&skills%5B%5D=1&skills%5B%5D=99&skills%5B%5D=124&skills%5B%5D=96'
+weddis_url = 'https://freelancehunt.com/projects?skills%5B%5D=41&skills%5B%5D=42&skills%5B%5D=43&skills%5B%5D=93&skills%5B%5D=17&skills%5B%5D=151'
 
 def parse_category(url, category):
     page = requests.get(url).content
@@ -48,3 +51,6 @@ def parse_category(url, category):
     session.commit()
 
 parse_category(admin_url, 'admin')
+parse_category(dev_url, 'dev')
+parse_category(webdev_url, 'webdev')
+parse_category(webdis_url, 'webdis')
