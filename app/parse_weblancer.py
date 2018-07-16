5bf9cb6107b33f87982c3b197df4e4423d5535ca
Maxim Semenov <maskim.semenov@visyond.com>
2018-07-16 08:37:29 +0300

    Weblancer parser added

--- app/parse_weblancer.py
+++ app/parse_weblancer.py
@@ -1,20 +1,71 @@
 # -*- coding: utf-8 -*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup          # Для обработки HTML
 
-import urllib2
-import re
-from BeautifulSoup import BeautifulSoup          # Для обработки HTML\
+from db import *
+from common import job_exist
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
 
 admin_url = 'https://www.weblancer.net/jobs/administrirovanie-sajtov-6/'
+dev_url = 'https://www.weblancer.net/jobs/programmirovanie-po-i-sistem-2/'
+webdev_url = 'https://www.weblancer.net/jobs/veb-programmirovanie-i-sajty-3/'
+webdis_url = 'https://www.weblancer.net/jobs/veb-dizajn-i-interfejsy-1/'
 
-page = urllib2.urlopen(admin_url)
-soup = BeautifulSoup(page)
 
-all_jobs = soup.findAll('div', {'class': 'row'})
+def parse_category(url, category):
+    page = requests.get(url).content
+    soup = BeautifulSoup(page, 'lxml')
+    all_jobs = soup.findAll('div', {'class': 'row'})
+    for job in all_jobs:
+        title_raw = job.find('div', class_='col-sm-10')
+        try:  # to get Title
+            title = title_raw.find('h2').text
+            url = 'https://www.weblancer.net' + title_raw.find('a').attrs['href']
+            if not job_exist(url):
+                text = title_raw.find('p').text
+                try:  # to get date
+                    date = job.find('span', class_='time_ago').attrs['data-timestamp']
+                    # from IPython import embed; embed(); import sys; sys.exit()
+                except AttributeError:
+                    date = job.find('span', class_='time_ago').attr['data-timestamp']
 
-for job in all_jobs:
-	title = job.find('h1')
-	print title
+                print(date)
+                try:  # to get price
+                    price = job.find('div', class_='amount').text
+                except AttributeError:
+                    price = None
+
+                print('\nDate:', date, \
+                      '\nTitle:', title, \
+                      '\nText:', text, \
+                      '\nPrice:', price, \
+                      '\nURL:', url)
+                job_row = Job(
+                    title=title,
+                    date=date,
+                    price=price,
+                    url=url,
+                    category=category,
+                    parse_date=datetime.now(),
+                    description=text
+                )
+                session.add(job_row)
+                session.commit()
+
+            else:
+                print(title)
+
+        except:
+            pass
+
+
+
+parse_category(admin_url, 'admin')
+parse_category(dev_url, 'dev')
+parse_category(webdev_url, 'webdev')
+parse_category(webdis_url, 'webdis')
