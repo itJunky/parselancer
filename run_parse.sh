90a62085f956f8b365e3a072e92183a32198954a
itj <you@example.com>
2019-11-07 16:22:15 +0300

    rm space

--- run_parse.sh
+++ run_parse.sh
@@ -6,7 +6,7 @@ if [ $already_running -eq 0 ]
 then
 	python2 ./app/parse_freelansim.py
 #	python2 ./app/parse_freelance_com.py  # doesn't work with anonymous access
-    python3 ./app/parse_weblancer.py
+  python3 ./app/parse_weblancer.py
 	python3 ./app/parse_freelancehunt.py
 else
 	echo "Already running $already_running jobs"
