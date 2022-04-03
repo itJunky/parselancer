da6582d1bdb638a5af1efaac91d2cdf06874d78d
Your Name <you@example.com>
2022-04-03 14:36:28 +0300

    Set low priority for parsers

--- run_parse.sh
+++ run_parse.sh
@@ -4,9 +4,9 @@
 already_running=`ps aux | grep parse_.*py | grep -v grep | wc -l`
 if [ $already_running -eq 0 ]
 then
-	python3 ./app/parse_freelance_habr.py
-	python3 ./app/parse_weblancer.py
-	python3 ./app/parse_freelancehunt.py
+	nice -n 10 python3 ./app/parse_freelance_habr.py
+	nice -n 10 python3 ./app/parse_weblancer.py
+	nice -n 10 python3 ./app/parse_freelancehunt.py
 else
 	echo "Already running $already_running jobs"
 fi
