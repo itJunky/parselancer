be812424b4cf1601592d1746ce20a692f4f590da
root <root@bot.ro0o.tk>
2022-09-03 21:30:45 +0300

    Fix freeze killer for skip other opened files

--- kill_freezed_parsers.sh
+++ kill_freezed_parsers.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-PID=`ps -eo pid,etime,cmd | grep parse_.*py | grep -v grep | awk '{print $1}'`
-START_TIME=`ps -eo pid,etime,cmd | grep parse_.*py | grep -v grep | awk '{print $2}' | awk -F ':' '{print $1}'`
+PID=`ps -eo pid,etime,cmd | grep " parse_.*py" | grep -v grep | awk '{print $1}'`
+START_TIME=`ps -eo pid,etime,cmd | grep " parse_.*py" | grep -v grep | awk '{print $2}' | awk -F ':' '{print $1}'`
 
-echo $PID
-echo $START_TIME
+#echo $PID
+#echo $START_TIME
 
 #START_TIME='12'
-echo $START_TIME | awk -F ':' '{print $1}'
+#echo $START_TIME | awk -F ':' '{print $1}'
 if [[ "$START_TIME" -ge 10 ]]
 then
+	echo $PID
+	echo $START_TIME
+	echo $START_TIME | awk -F ':' '{print $1}'
 	echo "Killing $START_TIME $PID"
 	kill $PID
 fi
