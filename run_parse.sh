bd22079b78c612932035320ea5f9524d0c9e38a8
root <root@bot.ro0o.tk>
2022-09-03 21:33:01 +0300

    Remove unneded debug output

--- run_parse.sh
+++ run_parse.sh
@@ -7,6 +7,6 @@ then
 	nice -n 10 python3 ./app/parse_freelance_habr.py
 	nice -n 10 python3 ./app/parse_weblancer.py
 	nice -n 10 python3 ./app/parse_freelancehunt.py
-else
-	echo "Already running $already_running jobs"
+#else
+#	echo "Already running $already_running jobs"
 fi
