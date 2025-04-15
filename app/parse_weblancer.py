a394917ae9242a0050157d4e9635039c530abf08
user <user@BOTS.local>
2025-04-15 13:10:35 +0000

    Fix weblancer parser

--- app/parse_weblancer.py
+++ app/parse_weblancer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import time
 from datetime import datetime
 from bs4 import BeautifulSoup  # Для обработки HTML
 
@@ -12,12 +13,18 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
+DEBUG = True
+
 admin_urls = [
     'https://www.weblancer.net/jobs/napolnenie-sajtov-35/',
     'https://www.weblancer.net/jobs/sistemnoe-administrirovanie-54/',
     'https://www.weblancer.net/jobs/sluzhba-podderzhki-56/'
 ]
-dev_url = 'https://www.weblancer.net/jobs/programmirovanie-po-i-sistem-2/'
+#dev_url = 'https://www.weblancer.net/jobs/programmirovanie-po-i-sistem-2/'
+#TODO Добавить ещё 5 урлов про программирование
+dev_urls = [
+    'https://www.weblancer.net/freelance/prikladnoe-po-23/'
+]
 webdev_url = 'https://www.weblancer.net/jobs/veb-programmirovanie-i-sajty-3/'
 webdis_url = 'https://www.weblancer.net/jobs/veb-dizajn-i-interfejsy-1/'
 
@@ -26,66 +33,77 @@ def parse_category(url, category):
     page = requests.get(url).content
     soup = BeautifulSoup(page, 'html.parser')
 
-    all_jobs = soup.find_all('div', class_='row click_container-link set_href')
+    all_jobs = soup.find_all('article')
+
+    if DEBUG is True:
+        # print(page)
+        # print(all_jobs_content)
+        # print(all_jobs[0])
+        print(len(all_jobs))
+        # return
 
     for job in all_jobs:
+        # time.sleep(0.200)
+
+        link = 'https://www.weblancer.net' + job.find('a').attrs['href']
+
+        if job_exist(link): continue
+
+        title = job.find('h2').text
+
         try:
-            right = job.find('div', class_='col-sm-4 text-sm-right').find('span')
-            if right.text.startswith('Закрыт'):
-                continue
+            text = " ".join(job.find('div', class_='text-rich').text.strip().split(" ")[:-1])
+        except AttributeError:
+            # todo что-то не актуальное
+            text = job.find('div', class_='text_field text-inline').text.strip()
 
+        text = text.replace("\n", " ")
+        
+        try:
+            price = job.find('div', class_='text-success').text
+        except AttributeError:
+            price = None
+
+        try:
+            # date = int(right.find('span', class_='time_ago').attrs['data-timestamp'])
+            date_raw = job.find('span', class_='ms-1')
+            date = date_raw.find('div').attrs['title']
         except:
-            pass
+            date = ''
 
-        a = job.find('div', class_='title').find('a')
-
-        title = a.text.strip()
-        url = 'https://www.weblancer.net' + a.attrs['href']
-
-        if not job_exist(url):
-            try:
-                date = int(right.find('span', class_='time_ago').attrs['data-timestamp'])
-            except:
-                date = ''
-            try:
-                text = " ".join(job.find('div', class_='collapse').text.strip().split(" ")[:-1])
-            except AttributeError:
-                text = job.find('div', class_='text_field text-inline').text.strip()
-
-            text = text.replace("\n", " ")
-
-            try:
-                price = job.find('div', class_='float-right float-sm-none title amount indent-xs-b0').find('span').text.strip()
-            except AttributeError:
-                price = None
-
-           #print('\nDate:', date, \
-           #        '\nTitle:', title, \
-           #        '\nText:', text, \
-           #        '\nPrice:', price, \
-           #        '\nURL:', url
-           #)
-
-            job_row = Job(
-                title=title,
-                date=date,
-                price=price,
-                url=url,
-                category=category,
-                parse_date=datetime.now(),
-                description=text
+        if DEBUG is True:
+            print('\nDate:', date, \
+                '\nTitle:', title, \
+                '\nText:', text, \
+                '\nPrice:', price, \
+                '\nURL:', url
             )
+            print('.' * 80)
 
-            session.add(job_row)
-            session.commit()
+        job_row = Job(
+            title=title,
+            date=date,
+            price=price,
+            url=url,
+            category=category,
+            parse_date=datetime.now(),
+            description=text
+        )
+
+        session.add(job_row)
+        session.commit()
+
 
         #else:
         #    print(title)
 
 
-for admin_url in admin_urls:
-    parse_category(admin_url, 'admin')
+#for admin_url in admin_urls:
+#    parse_category(admin_url, 'admin')
+for url in dev_urls:
+    parse_category(url, 'dev')
 
-parse_category(dev_url, 'dev')
-parse_category(webdev_url, 'webdev')
-parse_category(webdis_url, 'webdis')
+#parse_category(webdev_url, 'webdev')
+#parse_category(webdis_url, 'webdis')
+
+
